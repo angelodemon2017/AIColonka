d2424b5cec84326141e5a5c6ce1cf45e947bed58
angelodemon2017 <31762012+angelodemon2017@users.noreply.github.com>
2025-02-26 09:44:21 +0500

    Shader circle cutting wip

--- Assets/Scripts/LevelHelpers/ZoneTrigger.cs
+++ Assets/Scripts/LevelHelpers/ZoneTrigger.cs
@@ -3,33 +3,12 @@ using UnityEngine.Events;
 
 public class ZoneTrigger : MonoBehaviour
 {
-    [SerializeField] private TaskSO _needTask;
     [SerializeField] private UnityEvent _unityEvent;
     [SerializeField] private UnityEvent<GameObject> _enterObject;
-
-    private void Awake()
-    {
-//        ControllerDemoSaveFile.Instance.mainData.progressHistory.TaskUpdate += CheckAvailable;
-        CheckAvailable();
-    }
-
-    public void CheckAvailable()
-    {
-/*        if (_needTask == null)
-        {
-            ControllerDemoSaveFile.Instance.mainData.progressHistory.TaskUpdate -= CheckAvailable;
-            return;
-        }
-        else
-        {
-            gameObject.SetActive(ControllerDemoSaveFile.Instance.IsCurrentTask(_needTask));
-        }/**/
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.name == Dicts.SpecNames.Player)
-        //??if (other.tag == Dicts.Tags.Player)
         {
             RunScript();
             _enterObject?.Invoke(other.gameObject);
@@ -39,10 +18,5 @@ public class ZoneTrigger : MonoBehaviour
     private void RunScript()
     {
         _unityEvent?.Invoke();
-    }
-
-    private void OnDestroy()
-    {
-//        ControllerDemoSaveFile.Instance.mainData.progressHistory.TaskUpdate -= CheckAvailable;
     }
 }
